040f91d6e8f06fa3746aa1e35fbf97188204a6e7
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-06-13 22:52:47 -0300

    [autopep8] deezloader module

--- userbot/modules/deezloader.py
+++ userbot/modules/deezloader.py
@@ -3,14 +3,13 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# requires: deezloader hachoir Pillow
 # Ported from UniBorg by AnggaR96s
- 
+
 import deezloader
 import os
 import shutil
 import time
- 
+
 from userbot.events import register
 from userbot import CMD_HELP, DEEZER_ARL_TOKEN, TEMP_DOWNLOAD_DIRECTORY
 from hachoir.metadata import extractMetadata
@@ -18,43 +17,43 @@ from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeAudio
 
 
-@register(outgoing=True, pattern="^\.deez (.+?|) (FLAC|MP3\_320|MP3\_256|MP3\_128)")
+@register(outgoing=True,
+          pattern=r"^\.deez (.+?|) (FLAC|MP3\_320|MP3\_256|MP3\_128)")
 async def _(event):
     """DeezLoader by @An0nimia
     Ported for UniBorg by @SpEcHlDe"""
     if event.fwd_from:
         return
- 
+
     strings = {
         "name": "DeezLoad",
         "arl_token_cfg_doc": "`ARL Token for Deezer`",
         "invalid_arl_token": "`Please set the required variables for this module`",
         "wrong_cmd_syntax": "`Bruh, now i think how far should we go. please terminate my Session 🥺`",
         "server_error": "`We're experiencing technical difficulties.`",
-        "processing": "`Downloading..`"
-    }
- 
+        "processing": "`Downloading..`"}
+
     ARL_TOKEN = DEEZER_ARL_TOKEN
- 
+
     if ARL_TOKEN is None:
         await event.edit(strings["invalid_arl_token"])
         return
- 
+
     try:
         loader = deezloader.Login(ARL_TOKEN)
     except Exception as er:
         await event.edit(str(er))
         return
- 
+
     temp_dl_path = os.path.join(TEMP_DOWNLOAD_DIRECTORY, str(time.time()))
     if not os.path.exists(temp_dl_path):
         os.makedirs(temp_dl_path)
- 
+
     required_link = event.pattern_match.group(1)
     required_qty = event.pattern_match.group(2)
- 
+
     await event.edit(strings["processing"])
- 
+
     if "spotify" in required_link:
         if "track" in required_link:
             required_track = loader.download_trackspo(
@@ -68,7 +67,7 @@ async def _(event):
             await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
         elif "album" in required_link:
             reqd_albums = loader.download_albumspo(
                 required_link,
@@ -83,7 +82,7 @@ async def _(event):
                 await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
     elif "deezer" in required_link:
         if "track" in required_link:
             required_track = loader.download_trackdee(
@@ -97,7 +96,7 @@ async def _(event):
             await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
         elif "album" in required_link:
             reqd_albums = loader.download_albumdee(
                 required_link,
@@ -112,11 +111,11 @@ async def _(event):
                 await upload_track(required_track, event)
             shutil.rmtree(temp_dl_path)
             await event.delete()
- 
+
     else:
         await event.edit(strings["wrong_cmd_syntax"])
- 
- 
+
+
 async def upload_track(track_location, message):
     metadata = extractMetadata(createParser(track_location))
     duration = 0
@@ -151,9 +150,10 @@ async def upload_track(track_location, message):
     )
     os.remove(track_location)
 
- 
-CMD_HELP.update({"deezloader": ['DeezLoader',
-                               " - `deez <spotify/deezer link> <Music Format>`: Download music from deezer or spotify.\n\n"
-                               "**All commands can be used with** `.`"]
-                 })
 
+CMD_HELP.update(
+    {
+        "deezloader": [
+            'DeezLoader',
+            " - `deez <spotify/deezer link> <Music Format>`: Download music from deezer or spotify.\n\n"
+            "**All commands can be used with** `.`"]})
